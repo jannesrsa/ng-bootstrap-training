33678fe4f2d59c3e8ba831b746c1964eac3ce081
Jannes Coetzee <jannes@k2.com>
2019-10-08 10:42:44 +0200

    ngb-accordion

--- workout-tracker/src/app/home/home.component.ts
+++ workout-tracker/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public stepComplete: number = 0;
+  @ViewChild('acc', { static: false }) acc: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  nextButton(step: number) {
+    this.stepComplete = step;
+    this.acc.toggle(`panel${this.stepComplete + 1}`);
+  }
+
+  beforeChange($event): void {
+    if ($event.panelId === 'panel2' && this.stepComplete < 1) {
+      $event.preventDefault();
+    }
+
+    if ($event.panelId === 'panel3' && this.stepComplete < 2) {
+      $event.preventDefault();
+    }
   }
 
 }
